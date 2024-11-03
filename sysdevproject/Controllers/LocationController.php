d9c0932057a41d99310ba15312e1ec2ef8f8aa44
Vincent-C3 <70595609+Vincent-C3@users.noreply.github.com>
2024-11-03 18:31:49 -0500

    added location

---
+++ sysdevproject/Controllers/LocationController.php
@@ -0,0 +1,20 @@
+<?php
+include_once "Controllers/Controller.php";
+include_once "Models/LocationModel.php";
+
+class LocationController extends Controller
+{
+    function route()
+    {
+        $action = isset($_GET['action']) ? $_GET['action'] : "index";
+
+        if ($action == "index") {
+            $this->render("Location", "locations");
+        } elseif ($action == "YUL-Airport") {
+            $this->render("Location", "YUL-Airport");
+        } else {
+            $this->render("Location", "locations");
+        }
+    }
+}
+?>
