eca9528d656c799e45e30fd72f5d0f6c6d94cd2a
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-12-01 15:40:17 -0500

    updated navbar to make responsive

--- sysdevproject/Views/nav.php
+++ sysdevproject/Views/nav.php
@@ -30,63 +30,67 @@ include_once "Languages/".$language.".php";
 
             window.location.href = url;
         }
+        function showSidebar() {
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.style.display = 'flex';
+        }
+        function hideSidebar() {
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.style.display = 'none';
+        }
     </script>
     <header>
-        <div class="page-content-inner">
-            <div class="header-inner top">
-                <div class="section-left">
-                    <div class="header-nav logo">
-                        <?php if($controller != "admin"){ ?>
-                            <a href=<?php echo $path."/".$language?> id="logo">
-                                <span class="first-letter">C</span>yber <span class="second-letter">S</span>tation
-                            </a>
+        <nav>
+            <ul class="sidebar">
+                <li onclick=hideSidebar()><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="black"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
+                <li><a href="<?php echo $path ."/". $language . "/Location"; ?>" class="menu-items"><?=LOCATIONS?></a></li>
+                <li><a href="<?php echo $path ."/". $language . "/Contact"; ?>" class="menu-items"><?=CONTACTUS?></a></li>
+                <li><a href="<?php echo $path ."/". $language . "/Reservation"; ?>" class="menu-items"><?=RESERVE?></a></li>
+                <li>
+                    <select id="languages" onchange="changeLanguage(this.value)">
+                        <option disabled selected><?=LANGUAGES?></option>
+                        <option value='en' <?=$language=='en' ? 'selected' : '' ?>><?=ENGLISH?></option>
+                            <option value='fr' <?=$language=='fr' ? 'selected' : '' ?>><?=FRENCH?></option>
+                    </select>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <?php if($controller != "admin"){ ?>
+                        <a href=<?php echo $path."/".$language?> id="logo">
+                            <span class="first-letter">C</span>yber <span class="second-letter">S</span>tation
+                        </a>
                         <?php } elseif($controller=="admin"){ ?>
                             <a id="logo">
                                 <span class="first-letter">C</span>yber <span class="second-letter">S</span>tation
                             </a>
-                        <?php } ?>
-                    </div>
-                </div>
-                <div class="section-right">
-                    <?php if($controller != "admin"){ ?>
-                        <div class="header-nav menu-items">
-                            <a href=<?php echo $path ."/". $language . "/Location"; ?> id="account">
-                                <?=LOCATIONS?>
-                            </a>
-                        </div>
-                        <div class="header-nav menu-items">
-                            <a href=<?php echo $path ."/". $language . "/Contact"; ?> id="account">
-                                <?=CONTACTUS?>
-                            </a>
-                        </div>
-                        <div class="header-nav menu-items">
-                            <a href=<?php echo $path ."/". $language . "/Reservation"; ?> id="account">
-                                <?=RESERVE?>
-                            </a>
-                        </div>
-                        <div class="header-nav languages">
-                            <select id="languages" onchange="changeLanguage(this.value)">
-                                <option disabled selected><?=LANGUAGES?></option>
-                                <option value='en' <?=$language=='en' ? 'selected' : '' ?>><?=ENGLISH?></option>
-                                <option value='fr' <?=$language=='fr' ? 'selected' : '' ?>><?=FRENCH?></option>
-                            </select>
-                        </div>
-                    <?php }elseif($controller == "admin") {?>
-                        <div class="header-nav menu-items">
-                            <a href=<?php echo $path ."/". $language . "/Admin/logout"; ?> id="account">
-                                <?=LOGOUT?>
-                            </a>
-                        </div>
-                        <div class="header-nav languages">
-                            <select id="languages" onchange="changeLanguage(this.value)">
-                                <option disabled selected><?=LANGUAGES?></option>
-                                <option value='en' <?=$language=='en' ? 'selected' : '' ?>><?=ENGLISH?></option>
-                                <option value='fr' <?=$language=='fr' ? 'selected' : '' ?>><?=FRENCH?></option>
-                            </select>
-                        </div>
-                    <?php }?>
-                </div>
-            </div> 
-        </div>
+                    <?php } ?>
+                </li>
+                <li class="hideOnMobile"><a href="<?php echo $path ."/". $language . "/Location"; ?>" class="menu-items"><?=LOCATIONS?></a></li>
+                <li class="hideOnMobile"><a href="<?php echo $path ."/". $language . "/Contact"; ?>" class="menu-items"><?=CONTACTUS?></a></li>
+                <li class="hideOnMobile"><a href="<?php echo $path ."/". $language . "/Reservation"; ?>" class="menu-items"><?=RESERVE?></a></li>
+                <li class="hideOnMobile">
+                <?php if($controller != "admin"){ ?>
+                                <select id="languages" onchange="changeLanguage(this.value)">
+                                    <option disabled selected><?=LANGUAGES?></option>
+                                    <option value='en' <?=$language=='en' ? 'selected' : '' ?>><?=ENGLISH?></option>
+                                    <option value='fr' <?=$language=='fr' ? 'selected' : '' ?>><?=FRENCH?></option>
+                                </select>
+                        <?php }elseif($controller == "admin") {?>
+                            <li>
+                                <a href=<?php echo $path ."/". $language . "/Admin/logout"; ?> id="account">
+                                    <?=LOGOUT?>
+                                </a>
+                            </li>
+                                <select id="languages" onchange="changeLanguage(this.value)">
+                                    <option disabled selected><?=LANGUAGES?></option>
+                                    <option value='en' <?=$language=='en' ? 'selected' : '' ?>><?=ENGLISH?></option>
+                                    <option value='fr' <?=$language=='fr' ? 'selected' : '' ?>><?=FRENCH?></option>
+                                </select>
+                        <?php }?>
+                </li>
+                <li class="menu-button" onclick=showSidebar()><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="black"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
+            </ul>
+        </nav>
     </header>
 </html>
